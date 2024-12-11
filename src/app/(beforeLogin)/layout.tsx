df340b93a23a66139473dc14c12f9e0cbd84dda7
JJONG\ljm17 <ljm920604@gmail.com>
2024-12-11 22:47:23 +0900

    Feat. Intercepter Routing

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -11,3 +11,6 @@ export default function Layout({ children, modal }: Props) {
     </div>
   );
 }
+
+// 주소가 localhost:3001 일때는 children -> page.tsx , modal -> @mdaol/default.tsx
+// 주소가 localhost:3001/i/flow/login 때는 children -> i/flow/login/page.tsx , modal -> @modal/i/flow/login/page.tsx
--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from "next/navigation";
+import { redirect } from 'next/navigation';
 
 export default function Login() {
-  redirect("/i/flow/login");
+  redirect('/i/flow/login');
 }
